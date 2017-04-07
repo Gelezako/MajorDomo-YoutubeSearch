f8af2d8e9d7a465038aba2364a6fdf53a7e9af22
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-04-08 02:35:54 +0700

    Update search.php

--- modules/SearchYoutube/search.php
+++ modules/SearchYoutube/search.php
@@ -22,7 +22,7 @@ if (isset($_GET['q']) && isset($_GET['maxResults'])) {
    * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
    * Please ensure that you have enabled the YouTube Data API for your project.
    */
-  $DEVELOPER_KEY = 'AIzaSyDATl3ovDXqw5VcfNxUSBYOt5ku8KwIJz0';
+  $DEVELOPER_KEY = 'вставить сюда ваш гугл ключ';
 
   $client = new Google_Client();
   $client->setDeveloperKey($DEVELOPER_KEY);
