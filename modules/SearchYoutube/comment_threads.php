31d5c7d5831b68b43843c780eef3c35f256d4c82
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-04-08 02:43:39 +0700

    Update comment_threads.php

--- modules/SearchYoutube/comment_threads.php
+++ modules/SearchYoutube/comment_threads.php
@@ -33,8 +33,8 @@ session_start();
  * <https://developers.google.com/youtube/v3/guides/authentication>
  * Please ensure that you have enabled the YouTube Data API for your project.
  */
-$OAUTH2_CLIENT_ID = '221011195684-2r32tdq0iu1m386tb1fcnb1v1c7dt4hr.apps.googleusercontent.com';
-$OAUTH2_CLIENT_SECRET = 'g_oMeWtpYlekmHmvQckKt0Iv';
+$OAUTH2_CLIENT_ID = 'YOUR_OAUTH2_CLIENT_ID';
+$OAUTH2_CLIENT_SECRET = 'YOUR_OAUTH2_CLIENT_SECRET';
 
 /* You can replace $VIDEO_ID with one of your videos' id, channel id with your channel's id,
  * and text with the comment you want to be added.
